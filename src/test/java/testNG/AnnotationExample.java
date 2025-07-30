8278d3aa95a83310d47d601f0e3735fab214de91
Akila Senanayake <akilasenanayake22@gmail.com>
2025-07-30 15:06:56 +0530

    Add annotations files

---
+++ src/test/java/testNG/AnnotationExample.java
@@ -0,0 +1,45 @@
+package testNG;
+
+import org.testng.annotations.*;
+
+public class AnnotationExample {
+    @Test
+    public void test1() {
+        System.out.println("Im test 1");
+    }
+
+    @Test
+    public void test2(){
+        System.out.println("Im test 2");
+    }
+
+    @BeforeMethod
+    public void beforeMethod() {
+        System.out.println("Im before method");
+    }
+
+    @AfterMethod
+    public void afterMethod() {
+        System.out.println("Im after method");
+    }
+
+    @BeforeClass
+    public void beforeClass() {
+        System.out.println("Im before class");
+    }
+
+    @AfterClass
+    public void afterClass() {
+        System.out.println("Im after class");
+    }
+
+    @BeforeSuite
+    public void beforeSuite(){
+        System.out.println("Im before suite");
+    }
+
+    @AfterSuite
+    public void afterSuite(){
+        System.out.println("Im after suite");
+    }
+}
